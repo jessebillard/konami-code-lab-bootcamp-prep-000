2a33a321d4a86ebc025850bbadd0f7a743e4b11c
Jesse Billard <jessebillard@mac.com>
2018-02-26 21:18:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-
-function init () {
-  const body = document.body;
+const body = document.body;
   let index = 0;
+function init () {
+  
   
   body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
